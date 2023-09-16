9f199deb009d3bb37e4de6cdd27a437275775c08
Ron Le <ronle9519@gmail.com>
2023-09-16 19:24:59 +0700

    SR-35 feat: add email sender

--- Presentation/ServeSync.API/Controllers/AuthController.cs
+++ Presentation/ServeSync.API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ServeSync.API.Dtos.Auth;
+using ServeSync.Application.SeedWorks.MailSender;
 using ServeSync.Infrastructure.Identity.UseCases.Auth.Commands;
 using ServeSync.Infrastructure.Identity.UseCases.Auth.Dtos;
 
@@ -11,16 +12,25 @@ namespace ServeSync.API.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly IMediator _mediator;
+    private readonly IEmailSender _emailSender;
 
-    public AuthController(IMediator mediator)
+    public AuthController(IMediator mediator, IEmailSender emailSender)
     {
         _mediator = mediator;
+        _emailSender = emailSender;
     }
 
     [HttpPost("sign-in")]
     [ProducesResponseType(typeof(AuthCredentialDto), StatusCodes.Status200OK)]
     public async Task<IActionResult> SignInAsync(SignInCommand signInCommand)
     {
+        await _emailSender.SendAsync(new EmailMessage()
+        {
+            ToAddress = "ronle9519@gmail.com",
+            Subject = "Hello World",
+            Body = "<h1>Hello World</h1>"
+        });
+        
         var authCredential = await _mediator.Send(signInCommand);
         return Ok(authCredential);
     }
