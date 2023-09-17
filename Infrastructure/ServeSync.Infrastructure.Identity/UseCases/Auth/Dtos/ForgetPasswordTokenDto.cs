2cfd7ea653d9f03fed1f3e141270185a489b0f04
Ron Le <ronle9519@gmail.com>
2023-09-17 10:03:45 +0700

    SR-35 feat: add forget password token encode

---
+++ Infrastructure/ServeSync.Infrastructure.Identity/UseCases/Auth/Dtos/ForgetPasswordTokenDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ServeSync.Infrastructure.Identity.UseCases.Auth.Dtos;
+
+public class ForgetPasswordTokenDto
+{
+    public string Value { get; set; }
+    public string UserId { get; set; }
+}
