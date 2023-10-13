775a1718d7e361e48d02a53c80358c03e9dccc0b
Quytrw <trankimquy1952@gmail.com>
2023-10-13 21:35:22 +0700

    SR-176 feat: fetching a list of event categories

---
+++ Core/ServeSync.Application/UseCases/EventManagement/EventCategories/Dtos/EventCategoryDto.cs
@@ -0,0 +1,7 @@
+namespace ServeSync.Application.UseCases.EventManagement.EventCategories.Dtos;
+
+public class EventCategoryDto
+{
+    public Guid Id { get; set; }
+    public string Name { set; get; } = null!;
+}
