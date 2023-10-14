82f180f6290337cc5f6b871c12ed2d431d7a7d1a
Ron Le <ronle9519@gmail.com>
2023-10-14 17:40:52 +0700

    SR-167 feat: generate qr code for event attendance

---
+++ Core/ServeSync.Application/UseCases/EventManagement/Events/Jobs/GenerateAttendanceQrCodeBackGroundJob.cs
@@ -0,0 +1,20 @@
+﻿using ServeSync.Application.SeedWorks.Schedulers;
+using ServeSync.Domain.EventManagement.EventAggregate.Entities;
+
+namespace ServeSync.Application.UseCases.EventManagement.Events.Jobs;
+
+public class GenerateAttendanceQrCodeBackGroundJob : IBackGroundJob
+{
+    public IEnumerable<AttendanceInfoGenerateQrCodeBackGroundJobDto> AttendanceInfos { get; set; }
+    
+    public GenerateAttendanceQrCodeBackGroundJob(IEnumerable<AttendanceInfoGenerateQrCodeBackGroundJobDto> attendanceInfos)
+    {
+        AttendanceInfos = attendanceInfos;
+    }
+}
+
+public class AttendanceInfoGenerateQrCodeBackGroundJobDto
+{
+    public Guid Id { get; set; }
+    public string Code { get; set; } = null!;
+}
