4d2df76203e58660fec77ce17a46b76d8111c7d0
Le Ron <ronle9519@gmail.com>
2023-11-01 17:51:40 +0700

    feat: add status to registration & attendance info

--- Core/ServeSync.Application/UseCases/EventManagement/Events/Dtos/EventRegistrationInfos/EventRegistrationDto.cs
+++ Core/ServeSync.Application/UseCases/EventManagement/Events/Dtos/EventRegistrationInfos/EventRegistrationDto.cs
@@ -5,4 +5,30 @@ public class EventRegistrationDto
     public Guid Id { get; set; }
     public DateTime StartAt { get; set; }
     public DateTime EndAt { get; set; }
+    public RegistrationInfoStatus Status
+    {
+        get
+        {
+            var now = DateTime.Now;
+            if (now < StartAt)
+            {
+                return RegistrationInfoStatus.Upcoming;
+            }
+            else if (now > EndAt)
+            {
+                return RegistrationInfoStatus.Done;
+            }
+            else
+            {
+                return RegistrationInfoStatus.Happening;
+            }
+        }
+    }
+}
+
+public enum RegistrationInfoStatus
+{
+    Done,
+    Happening,
+    Upcoming
 }
