66deb94e61e6307ea7f1cc94c44190f1a82602b7
Le Ron <ronle9519@gmail.com>
2023-11-06 19:25:53 +0700

    feat: add upload asset image end point

--- Core/ServeSync.Application/ImageUploader/IImageUploader.cs
+++ Core/ServeSync.Application/ImageUploader/IImageUploader.cs
@@ -3,6 +3,8 @@
 public interface IImageUploader
 {
     Task<UploaderResult> UploadAsync(string name, Stream stream);
+    
+    Task<UploaderResult> UploadAssetAsync(string name, Stream stream);
 
     void PushUpload(string name, Stream stream);
 }
