ec575cb4f36145a13c488a34be1285dd54a268ff
Quytrw <trankimquy1952@gmail.com>
2023-11-11 22:50:36 +0700

    SR-215 feat: implement api post event organization

---
+++ Core/ServeSync.Application/UseCases/EventManagement/EventOrganizations/Commands/CreateEventOrganizationCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using ServeSync.Application.SeedWorks.Cqrs;
+using ServeSync.Application.SeedWorks.Data;
+using ServeSync.Domain.EventManagement.EventOrganizationAggregate;
+using ServeSync.Domain.EventManagement.EventOrganizationAggregate.DomainServices;
+
+namespace ServeSync.Application.UseCases.EventManagement.EventOrganizations.Commands;
+
+public class CreateEventOrganizationCommandHandler : ICommandHandler<CreateEventOrganizationCommand, Guid>
+{
+    private readonly IEventOrganizationRepository _eventOrganizationRepository;
+    private readonly IEventOrganizationDomainService _eventOrganizationDomainService;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger<CreateEventOrganizationCommandHandler> _logger;
+
+    public CreateEventOrganizationCommandHandler(
+        IEventOrganizationRepository eventOrganizationRepository,
+        IEventOrganizationDomainService eventOrganizationDomainService,
+        IUnitOfWork unitOfWork,
+        ILogger<CreateEventOrganizationCommandHandler> logger)
+    {
+        _eventOrganizationRepository = eventOrganizationRepository;
+        _eventOrganizationDomainService = eventOrganizationDomainService;
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+    
+    public async Task<Guid> Handle(CreateEventOrganizationCommand request, CancellationToken cancellationToken)
+    {
+        var eventOrganization = await _eventOrganizationDomainService.CreateAsync(
+            request.EventOrganization.Name,
+            request.EventOrganization.Email,
+            request.EventOrganization.PhoneNumber,
+            request.EventOrganization.ImageUrl,
+            request.EventOrganization.Description,
+            request.EventOrganization.Address);
+        
+        await _eventOrganizationRepository.InsertAsync(eventOrganization);
+        await _unitOfWork.CommitAsync();
+        
+        _logger.LogInformation("Created new event organization with id: {EventOrganizationId}", eventOrganization.Id);
+        return eventOrganization.Id;
+    }
+}
---
+++ Core/ServeSync.Domain/EventManagement/EventOrganizationAggregate/Specifications/EventOrganizationByNameSpecification.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using ServeSync.Domain.EventManagement.EventOrganizationAggregate.Entities;
+using ServeSync.Domain.SeedWorks.Specifications;
+
+namespace ServeSync.Domain.EventManagement.EventOrganizationAggregate.Specifications;
+
+public class EventOrganizationByNameSpecification : Specification<EventOrganization, Guid>
+{
+    private readonly string _name;
+    
+    public EventOrganizationByNameSpecification(string name)
+    {
+        _name = name;
+    }
+    
+    public override Expression<Func<EventOrganization, bool>> ToExpression()
+    {
+        return x => x.Name == _name;
+    }
+}
