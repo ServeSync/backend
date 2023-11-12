e3b64a05b365b93586f1f11d7908a06ac3f8000c
Le Ron <ronle9519@gmail.com>
2023-11-12 13:31:34 +0700

    fix: seed permission for role

--- Infrastructure/ServeSync.Infrastructure.Identity/Commons/Constants/EventOrganizationPermissions.cs
+++ Infrastructure/ServeSync.Infrastructure.Identity/Commons/Constants/EventOrganizationPermissions.cs
@@ -5,12 +5,19 @@ public static class EventOrganizationPermissions
     public static List<string> Provider = new()
     {
         Permissions.Users.ViewProfile,
-            
+        
         Permissions.Students.View,
         
         Permissions.Events.View,
+        Permissions.Events.Delete,
+        Permissions.Events.Create,
+        Permissions.Events.Edit,
+        Permissions.Events.Cancel,
+        Permissions.Events.ApproveRegistration,
+        Permissions.Events.RejectRegistration,
         
         Permissions.EventOrganizations.View,
+        Permissions.EventOrganizations.Update,
         
         Permissions.EventCollaborationRequests.View,
     };
--- Infrastructure/ServeSync.Infrastructure.Identity/Commons/Constants/EventOrganizerPermissions.cs
+++ Infrastructure/ServeSync.Infrastructure.Identity/Commons/Constants/EventOrganizerPermissions.cs
@@ -5,19 +5,12 @@ public static class EventOrganizerPermissions
     public static List<string> Provider = new()
     {
         Permissions.Users.ViewProfile,
-        
+            
         Permissions.Students.View,
         
         Permissions.Events.View,
-        Permissions.Events.Delete,
-        Permissions.Events.Create,
-        Permissions.Events.Edit,
-        Permissions.Events.Cancel,
-        Permissions.Events.ApproveRegistration,
-        Permissions.Events.RejectRegistration,
         
         Permissions.EventOrganizations.View,
-        Permissions.EventOrganizations.Update,
         
         Permissions.EventCollaborationRequests.View,
     };
