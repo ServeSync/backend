b6e3da5ea2bb9e1ebec8f7a56a885ea10e5040e0
Le Ron <ronle9519@gmail.com>
2023-11-18 19:38:53 +0700

    SR-265 feat: change password

---
+++ Infrastructure/ServeSync.Infrastructure.Identity/UseCases/Auth/Commands/ChangePasswordCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using ServeSync.Application.SeedWorks.Cqrs;
+using ServeSync.Application.SeedWorks.Sessions;
+using ServeSync.Domain.SeedWorks.Exceptions.Resources;
+using ServeSync.Infrastructure.Identity.Models.UserAggregate.Entities;
+using ServeSync.Infrastructure.Identity.Models.UserAggregate.Exceptions;
+
+namespace ServeSync.Infrastructure.Identity.UseCases.Auth.Commands;
+
+public class ChangePasswordCommandHandler : ICommandHandler<ChangePasswordCommand>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly ICurrentUser _currentUser;
+    private readonly ILogger<ChangePasswordCommandHandler> _logger;
+    
+    public ChangePasswordCommandHandler(
+        ICurrentUser currentUser, 
+        UserManager<ApplicationUser> userManager,
+        ILogger<ChangePasswordCommandHandler> logger)
+    {
+        _currentUser = currentUser;
+        _userManager = userManager;
+        _logger = logger;
+    }
+    
+    public async Task Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userManager.FindByIdAsync(_currentUser.Id);
+        if (user == null)
+        {
+            throw new UserNotFoundException(_currentUser.Id);
+        }
+
+        var result = await _userManager.ChangePasswordAsync(user, request.CurrentPassword, request.NewPassword);
+        if (!result.Succeeded)
+        {
+            var error = result.Errors.First();
+            throw new ResourceInvalidOperationException(error.Description, error.Code);
+        }
+    }
+}
