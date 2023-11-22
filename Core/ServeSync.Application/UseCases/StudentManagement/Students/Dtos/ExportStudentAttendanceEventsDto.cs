47ada1e598b4d5f81916c36fcbe61b141fd728b3
Le Ron <ronle9519@gmail.com>
2023-11-22 19:39:14 +0700

    fix: change to post method

---
+++ Core/ServeSync.Application/UseCases/StudentManagement/Students/Dtos/ExportStudentAttendanceEventsDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ServeSync.Application.UseCases.StudentManagement.Students.Dtos;
+
+public class ExportStudentAttendanceEventsDto
+{
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate { get; set; }
+}
