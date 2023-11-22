9fced8e2bc229aa66b5e654171116edbd9e796c1
Le Ron <ronle9519@gmail.com>
2023-11-22 22:48:35 +0700

    feat: update organization contact query model

--- Core/ServeSync.Application/UseCases/EventManagement/EventOrganizations/Dtos/EventOrganizationContactFilterRequestDto.cs
+++ Core/ServeSync.Application/UseCases/EventManagement/EventOrganizations/Dtos/EventOrganizationContactFilterRequestDto.cs
@@ -1,8 +1,10 @@
 ﻿using ServeSync.Application.Common.Dtos;
+using ServeSync.Domain.EventManagement.EventOrganizationAggregate.Enums;
 
 namespace ServeSync.Application.UseCases.EventManagement.EventOrganizations.Dtos;
 
 public class EventOrganizationContactFilterRequestDto : PagingAndSortingRequestDto
 {
+    public OrganizationStatus? Status { get; set; }
     public string? Search { get; set; }
 }
--- Core/ServeSync.Application/UseCases/EventManagement/EventOrganizations/Queries/GetAllEventOrganizationContactQuery.cs
+++ Core/ServeSync.Application/UseCases/EventManagement/EventOrganizations/Queries/GetAllEventOrganizationContactQuery.cs
@@ -1,21 +1,25 @@
 ﻿using ServeSync.Application.Common.Dtos;
 using ServeSync.Application.SeedWorks.Cqrs;
 using ServeSync.Application.UseCases.EventManagement.EventOrganizations.Dtos;
+using ServeSync.Domain.EventManagement.EventOrganizationAggregate.Enums;
 
 namespace ServeSync.Application.UseCases.EventManagement.EventOrganizations.Queries;
 
 public class GetAllEventOrganizationContactQuery : PagingAndSortingRequestDto, IQuery<PagedResultDto<EventOrganizationContactDto>>
 {
     public Guid EventOrganizationId { get; set; }
+    public OrganizationStatus? Status { get; set; }
     public string? Search { get; set; }
     
     public GetAllEventOrganizationContactQuery(
         Guid eventOrganizationId, 
+        OrganizationStatus? status,
         string? search, 
         int page, 
         int size, 
         string? sorting) : base(page, size, sorting)
     {
+        Status = status;
         EventOrganizationId = eventOrganizationId;
         Search = search;
     }
