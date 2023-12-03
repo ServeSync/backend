2cbadb38359852c22513d252bd5df656bb7f6101
Le Ron <ronle9519@gmail.com>
2023-12-03 17:32:33 +0700

    feat: approve proof

---
+++ Core/ServeSync.Application/UseCases/StudentManagement/Proofs/Commands/ApproveProofCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using ServeSync.Application.SeedWorks.Cqrs;
+using ServeSync.Application.SeedWorks.Data;
+using ServeSync.Domain.StudentManagement.ProofAggregate;
+using ServeSync.Domain.StudentManagement.ProofAggregate.DomainServices;
+using ServeSync.Domain.StudentManagement.ProofAggregate.Exceptions;
+
+namespace ServeSync.Application.UseCases.StudentManagement.Proofs.Commands;
+
+public class ApproveProofCommandHandler : ICommandHandler<ApproveProofCommand>
+{
+    private readonly IProofRepository _proofRepository;
+    private readonly IProofDomainService _proofDomainService;
+    private readonly ILogger<ApproveProofCommandHandler> _logger;
+    private readonly IUnitOfWork _unitOfWork;
+    
+    public ApproveProofCommandHandler(
+        IProofRepository proofRepository, 
+        IProofDomainService proofDomainService,
+        IUnitOfWork unitOfWork,
+        ILogger<ApproveProofCommandHandler> logger)
+    {
+        _proofRepository = proofRepository;
+        _proofDomainService = proofDomainService;
+        _logger = logger;
+        _unitOfWork = unitOfWork;
+    }
+    
+    public async Task Handle(ApproveProofCommand request, CancellationToken cancellationToken)
+    {
+        var proof = await _proofRepository.FindByIdAsync(request.Id);
+        if (proof == null)
+        {
+            throw new ProofNotFoundException(request.Id);
+        }
+
+        _proofDomainService.ApproveProof(proof);
+        _proofRepository.Update(proof);
+        await _unitOfWork.CommitAsync();
+        
+        _logger.LogInformation("Proof {Id} approved successfully!", request.Id);
+    }
+}
--- Core/ServeSync.Domain/StudentManagement/ProofAggregate/ErrorCodes.cs
+++ Core/ServeSync.Domain/StudentManagement/ProofAggregate/ErrorCodes.cs
@@ -4,6 +4,6 @@ public static class ErrorCodes
 {
     public const string InternalProofAlreadyExist = "Proof:000001";
     public const string ProofNotFound = "Proof:000002";
-    public const string ProofCanNotBeRejected = "Proof:000003";
+    public const string ProofNotPending = "Proof:000003";
     public const string ProofCanNotBeDeleted = "Proof:000004";
 }
