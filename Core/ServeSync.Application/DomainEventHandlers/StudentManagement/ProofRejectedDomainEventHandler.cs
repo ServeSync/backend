1be2c3b0c24df4565e3701d1e47d60def54141ce
Quytrw <trankimquy1952@gmail.com>
2023-12-03 21:29:25 +0700

    SR-345 feat: implement api get proofs of student

--- Core/ServeSync.Application/DomainEventHandlers/StudentManagement/ProofRejectedDomainEventHandler.cs
+++ Core/ServeSync.Application/DomainEventHandlers/StudentManagement/ProofRejectedDomainEventHandler.cs
@@ -3,23 +3,29 @@ using ServeSync.Application.MailSender;
 using ServeSync.Application.MailSender.Interfaces;
 using ServeSync.Application.SeedWorks.Data;
 using ServeSync.Domain.SeedWorks.Events;
+using ServeSync.Domain.SeedWorks.Repositories;
 using ServeSync.Domain.StudentManagement.ProofAggregate.DomainEvents;
+using ServeSync.Domain.StudentManagement.StudentAggregate.Entities;
+using ServeSync.Domain.StudentManagement.StudentAggregate.Exceptions;
 
 namespace ServeSync.Application.DomainEventHandlers.StudentManagement;
 
 public class ProofRejectedDomainEventHandler : IDomainEventHandler<ProofRejectedDomainEvent>
 {
+    private readonly IBasicReadOnlyRepository<Student, Guid> _studentRepository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IEmailSender _emailSender;
     private readonly IEmailTemplateGenerator _emailTemplateGenerator;
     private readonly ILogger<ProofRejectedDomainEventHandler> _logger;
     
     public ProofRejectedDomainEventHandler(
+        IBasicReadOnlyRepository<Student, Guid> studentRepository,
         IUnitOfWork unitOfWork,
         IEmailSender emailSender,
         IEmailTemplateGenerator emailTemplateGenerator,
         ILogger<ProofRejectedDomainEventHandler> logger)
     {
+        _studentRepository = studentRepository;
         _unitOfWork = unitOfWork;
         _emailSender = emailSender;
         _emailTemplateGenerator = emailTemplateGenerator;
@@ -29,11 +35,17 @@ public class ProofRejectedDomainEventHandler : IDomainEventHandler<ProofRejected
     public async Task Handle(ProofRejectedDomainEvent notification, CancellationToken cancellationToken)
     {
         var eventName = notification.Proof.ExternalProof?.EventName ?? notification.Proof.InternalProof!.Event!.Name;
+        var student = await _studentRepository.FindByIdAsync(notification.Proof.StudentId);
+        if (student == null)
+        {
+            throw new StudentNotFoundException(notification.Proof.StudentId);
+        }
+        
         _emailSender.Push(new EmailMessage()
         {
-            ToAddress = notification.Proof.Student!.Email,
+            ToAddress = student.Email,
             Subject = "[ServeSync] Thông báo duyệt minh chứng",
-            Body = _emailTemplateGenerator.GetRejectProof(notification.Proof.Student.FullName, eventName, notification.RejectReason)
+            Body = _emailTemplateGenerator.GetRejectProof(student.FullName, eventName, notification.RejectReason)
         });
     }
 }
--- Core/ServeSync.Domain/StudentManagement/ProofAggregate/DomainEvents/ProofRejectedDomainEvent.cs
+++ Core/ServeSync.Domain/StudentManagement/ProofAggregate/DomainEvents/ProofRejectedDomainEvent.cs
@@ -6,7 +6,6 @@ namespace ServeSync.Domain.StudentManagement.ProofAggregate.DomainEvents;
 public class ProofRejectedDomainEvent : IDomainEvent
 {
     public Proof Proof { get; set;  }
-    
     public string RejectReason { get; set; }
     public ProofRejectedDomainEvent(Proof proof, string rejectReason)
     {
