a289ac09a965d6e780ffcd1ca3956efcd1b316ce
Quytrw <trankimquy1952@gmail.com>
2023-12-23 17:02:31 +0700

    SR-438 feat: implement api get all users

---
+++ Infrastructure/ServeSync.Infrastructure.Identity/UseCases/Users/Queries/GetAllUsersQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using ServeSync.Application.Common.Dtos;
+using ServeSync.Application.SeedWorks.Cqrs;
+using ServeSync.Domain.SeedWorks.Specifications.Interfaces;
+using ServeSync.Infrastructure.Identity.Models.UserAggregate;
+using ServeSync.Infrastructure.Identity.Models.UserAggregate.Entities;
+using ServeSync.Infrastructure.Identity.Models.UserAggregate.Specifications;
+using ServeSync.Infrastructure.Identity.UseCases.Users.Dtos;
+
+namespace ServeSync.Infrastructure.Identity.UseCases.Users.Queries;
+
+public class GetAllUsersQueryHandler: IQueryHandler<GetAllUsersQuery, PagedResultDto<UserBasicInfoDto>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+    
+    public GetAllUsersQueryHandler(
+        IUserRepository userRepository,
+        IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
+    
+    public async Task<PagedResultDto<UserBasicInfoDto>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+    {
+        var specification = GetSpecification(request);
+        var users = await _userRepository.GetPagedListAsync(specification);
+        var total = await _userRepository.GetCountAsync(specification);
+        return new PagedResultDto<UserBasicInfoDto>(
+            total,
+            request.Size,
+            _mapper.Map<IEnumerable<UserBasicInfoDto>>(users));
+    }
+    
+    private IPagingAndSortingSpecification<ApplicationUser, string> GetSpecification(GetAllUsersQuery request)
+    {
+        var specification = new FilterApplicationUserSpecification(request.Page, request.Size, request.Sorting, request.Search);
+        return specification;
+    }
+}
--- Presentation/ServeSync.API/Controllers/UserController.cs
+++ Presentation/ServeSync.API/Controllers/UserController.cs
@@ -1,10 +1,13 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ServeSync.API.Authorization;
+using ServeSync.Application.Common.Dtos;
 using ServeSync.Infrastructure.EfCore.Migrations;
 using ServeSync.Infrastructure.Identity.Commons.Constants;
 using ServeSync.Infrastructure.Identity.UseCases.Permissions.Dtos;
 using ServeSync.Infrastructure.Identity.UseCases.Permissions.Queries;
+using ServeSync.Infrastructure.Identity.UseCases.Users.Dtos;
+using ServeSync.Infrastructure.Identity.UseCases.Users.Queries;
 
 namespace ServeSync.API.Controllers;
 
@@ -27,4 +30,16 @@ public class UserController : ControllerBase
         var permissions = await _mediator.Send(new GetAllPermissionForUserQuery(id, tenantId));
         return Ok(permissions);
     }
+    
+    [HttpGet]
+    // [HasPermission(Permissions.Users.View)]
+    [ProducesResponseType(typeof(PagedResultDto<UserBasicInfoDto>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllUsersAsync([FromQuery] UserRequestDto dto)
+    {
+        var query = new GetAllUsersQuery(dto.Search, dto.Page, dto.Size, dto.Sorting);
+        
+        var users = await _mediator.Send(query);
+        return Ok(users);
+    }
+    
 }
