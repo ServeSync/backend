d100b2b95d0df94ca93f7fd7dc312348954dbd4c
Le Ron <ronle9519@gmail.com>
2024-01-06 01:10:13 +0700

    fix: fix refresh token

--- Infrastructure/ServeSync.Infrastructure.EfCore/Repositories/UserRepository.cs
+++ Infrastructure/ServeSync.Infrastructure.EfCore/Repositories/UserRepository.cs
@@ -37,6 +37,7 @@ public class UserRepository : EfCoreRepository<ApplicationUser, string>, IUserRe
                                     .ThenInclude(x => x.Tenants)
                                .Include(x => x.User)
                                     .ThenInclude(x => x.Roles)
+                                    .ThenInclude(x => x.Role)
                                .Include(x => x.User)
                                     .ThenInclude(x => x.RefreshToken)
                                .FirstOrDefaultAsync(x => x.Value == refreshToken))?.User;
